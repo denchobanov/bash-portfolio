3b2ce71ac4fe93d8505325f95f3e48e2cce04226
denchobanov <denischobanov669@gmail.com>
2026-06-11 13:37:44 +0300

    Add an option for backup and error handling

--- bash/system_health/system-health.sh
+++ bash/system_health/system-health.sh
@@ -10,8 +10,13 @@ MEM=$(free | awk '/Mem:/ {printf("%.0f", $3/$2 * 100)}')
 echo "Please enter where do you want the .log file saved:"
 read DEST
 mkdir -p "$DEST"
+if [ ! -d $DEST ]
+then
+    echo "Error! Thats not a directory!"
+    exit 1
+fi
 LOGFILE="$DEST/system-health.log"
-
+BACKUP="$DEST/backup_$TIME.log"
 echo "===== SYSTEM CHECK =====" | tee -a "$LOGFILE"
 echo "Time: $TIME" | tee -a "$LOGFILE"
 echo "Disk usage: $DISK%" | tee -a "$LOGFILE"
@@ -36,3 +41,12 @@ echo "========================" | tee -a "$LOGFILE"
 
 touch "$LOGFILE"
 echo ".log file saved to $LOGFILE"
+read -p "Would you like to save a backup? y/n: " ANSWER
+if [[ "$ANSWER" != "y"  && "$ANSWER" != "n" ]]
+then 
+    echo "Wrong input! Backup not saved!"
+    exit 1
+else
+    cp "$LOGFILE" "$BACKUP"
+    echo "Backup created successfully!"
+fi
